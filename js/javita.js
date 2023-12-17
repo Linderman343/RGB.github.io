b5aaa148571665cbb74706d72905704f363ed498
Leonel Gonzalez <leonelpala234@gmail.com>
2023-12-17 02:17:47 -0300

    Version 1.0.0 (No responsive)

--- js/javita.js
+++ js/javita.js
@@ -2,9 +2,32 @@ window.addEventListener('load', function(){
     let Red = document.querySelector('#Red')
     let Blue = document.querySelector('#Blue')
     let Green = document.querySelector('#Green')
+    
+    let RanDef = random()
+    
+    let btnNewColor = document.querySelector('#Newcolor')
+    btnNewColor.addEventListener('click', e=>{
+        RanDef = random()    
+    })
+    let btnComparar = document.querySelector('#comparar')
+    btnComparar.addEventListener('click', e =>{
+        let RGBdef = [Red.value,Green.value,Blue.value]
+        let diferencia = calcularDiferencia(RGBdef, RanDef);
+        let porcentajeParecido = 100 - (diferencia / (255 * 3) * 100);
+        resultado(porcentajeParecido.toFixed(2) + '%')
+    });
 
-    random()
+    function calcularDiferencia(rgb1, rgb2) {
+        let diferencia = 0;
+        for (let i = 0; i < 3; i++) {
+            diferencia += Math.abs(rgb1[i] - rgb2[i]);
+        }
+        return diferencia;
+    }
+
+
  
+
     Red.addEventListener('input',function() {
         refresh()
     })
@@ -19,20 +42,37 @@ window.addEventListener('load', function(){
         let RGBdef = `RGB(${Red.value},${Green.value},${Blue.value})`
         let caja = document.querySelector('#cubo');
         caja.style.background = RGBdef;
-        console.log(RGBdef)
-        document.querySelector('#rgbRan').innerHTML = RGBdef;
     }
 
     function random(){
-        console.log('hola')
         let RandomBlue = Math.floor(Math.random()* (256 - 0) + 0);
         let RandomRed = Math.floor(Math.random()* (256 - 0) + 0);
         let RandomGreen = Math.floor(Math.random()* (256 - 0) + 0);
-
         let RandomDef = `RGB(${RandomRed},${RandomGreen},${RandomBlue})`
-        document.querySelector('#rgbDef').innerHTML = RandomDef;
+        let Ranciada = [RandomRed, RandomGreen, RandomBlue ]
         let cajaDef = document.querySelector('#cuboRandom'); 
         cajaDef.style.background = RandomDef;
-        console.log(RandomDef)
+        return Ranciada
+    }
+    let a = 1
+    let pizzara = document.querySelector('.pizarra')
+    let btnpizarra = document.querySelector('#btnPizarra')
+    btnpizarra.addEventListener('click', e=>{
+        if (a == 0) {
+            pizzara.classList.add('show');
+            btnpizarra.classList.add('show');
+            a = 1
+        }else{
+            pizzara.classList.remove('show');
+            btnpizarra.classList.remove('show');
+            a = 0
+        }
+        
+    })
+
+    let res = document.querySelector('#resultados')
+    function resultado(x){
+        res.innerHTML += `<div style="background: rgb(${Red.value},${Green.value},${Blue.value});" class="color">rgb(${Red.value},${Green.value},${Blue.value})</div> <div style="background:rgb(${RanDef[0]},${RanDef[1]},${RanDef[2]});" class="color">rgb(${RanDef[0]},${RanDef[1]},${RanDef[2]})</div><span>${x}</span>`
+        RanDef=random()
     }
 })
